f53fb92ed28911aa385c753122644dff0fb8f3c2
Mahmud Mai <mahmudx00@gmail.com>
2023-07-14 18:06:47 +0100

    initial server commit

---
+++ portfolio-mahmud-server/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '8cj91w0f',
+    dataset: 'production'
+  }
+})
---
+++ portfolio-mahmud-server/schemas/index.js
@@ -0,0 +1,19 @@
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import testimonials from './testimonials'
+import skills from './skills'
+import works from './works'
+import workExperience from './workExperience'
+
+export const schemaTypes = [
+  about,
+  brands,
+  contact,
+  experiences,
+  testimonials,
+  works,
+  workExperience,
+  skills,
+]
